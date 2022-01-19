cdd18bf2675aa1f245b15de1b18ff5f26eb73dae
kennyLepe <96542019+kennyLepe@users.noreply.github.com>
2022-01-19 00:27:18 -0600

    Add files via upload

---
+++ Ejercicio_1.js
@@ -0,0 +1,16 @@
+//Part 1
+var clientes = ["Sofia","David","Juan"];
+
+//Part 2
+clientes.push("Sara","Agustin");
+
+clientes.shift();
+
+console.log(clientes); 
+
+//Part 3
+clientes.splice(1,0, "Renata");
+
+clientes.push("Elena");
+
+console.log(clientes);
---
+++ Ejercicio_3.js
@@ -0,0 +1,44 @@
+//Part 3
+var xValue = 3;  
+
+while(xValue > 0)
+{
+    console.log(xValue);
+    xValue = xValue - 0.5;
+}
+
+// ODD 
+
+var x = 1;  
+
+while(x < 101)
+{
+    console.log(x);
+    x = x + 2;
+
+ 
+}
+
+//Square brackets
+
+var d = 1;
+
+    while (d < 11 ) {
+        console.log( "[" +d+ "]" );
+        d = d + 1;
+
+    }
+
+// Bucle Sumas
+
+var n = 5;
+var suma = 0;
+ 
+while(n > 0) 
+{
+    console.log(n);
+    suma = suma + n; 
+    n = n - 1; 
+}
+
+console.log(suma);
